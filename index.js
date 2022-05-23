c3b409f2758948b9185ae3489012ca907c7bfa78
Caroline <ckmcdon@gmail.com>
2022-05-23 10:14:42 -0400

    commit tree 8c14cee826d78bd53814a5b91da7d90935b650d5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function (p) {
+    document.querySelector('#text').textContent = "This is really cool!"
+    console.log("The DOM has loaded");
+})
+
+
+
+//Use the event's callback function to target the paragraph 
+//element with id="text" and replace the text with "This is really cool!"
